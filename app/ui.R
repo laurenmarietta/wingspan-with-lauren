df3d6147c5a5cd0467c1e16ce6f34a9313e9f591
Lauren Marietta Chambers <lauren.m.chambers96@gmail.com>
2021-02-17 21:18:22 -0700

    Add spinners, footer

--- app/ui.R
+++ app/ui.R
@@ -1,5 +1,7 @@
 
 library(shiny)
+library(shinycssloaders)
+library(plotly)
 
 shinyUI(fluidPage(theme = "wingspan_with_lauren.css",
                   title="Wingspan with Lauren",
@@ -9,27 +11,35 @@ shinyUI(fluidPage(theme = "wingspan_with_lauren.css",
     tabsetPanel(type="pills",
         tabPanel("Overall", 
                  h3("Points over Time"),
-                 plotlyOutput("all_points_time"),
+                 withSpinner(plotlyOutput("all_points_time"), type=4, color="#b5b5b5", size=0.5),
                  h3("Ranking over Time"),
-                 plotlyOutput("rank_time"),
+                 withSpinner(plotlyOutput("rank_time"), type=4, color="#b5b5b5", size=0.5),
                  h3("Point Percentage per Game"),
-                 plotlyOutput("point_pct_time"),
+                 withSpinner(plotlyOutput("point_pct_time"), type=4, color="#b5b5b5", size=0.5),
                  h3("Distribution of Points"),
-                 plotlyOutput("box_whisker"),
+                 withSpinner(plotlyOutput("box_whisker"), type=4, color="#b5b5b5", size=0.5),
                  h3("Average Point Percentage per Player"),
-                 plotlyOutput("point_pct_freq_players")),
+                 withSpinner(plotlyOutput("point_pct_freq_players"), type=4, color="#b5b5b5", size=0.5)),
         
         tabPanel("By Player", 
                  selectInput("player_comp_select",
                              "Choose a player:",
                              c("Lauren", "Kira", "Julia")),
                  h3("Point Categories over Time"),
-                 plotlyOutput("player_comp"))
+                 withSpinner(plotlyOutput("player_comp"), type=4, color="#b5b5b5", size=0.5))
     ),
-    
+    hr(),
     p("Fonts:",
     a("Cardenio Modern Bold", href="https://www.dafont.com/cardenio-modern.font"),
     ",",
     a("Bebas Neue Regular", href="https://fonts.google.com/specimen/Bebas+Neue?preview.text_type=custom")
-    )
+    ),
+    p("Color Palette:",
+      a("Paul Tol's Accessible Color Schemes", href="https://personal.sron.nl/~pault/")),
+    p("Source:",
+      a("Google Sheets", 
+        href="https:\\docs.google.com/spreadsheets/d/1Ryei71k-te0LNubhoaB_HcQ1mWMH86r-bEghjvOmoNk/edit?usp=sharing")),
+    p("Github:",
+      a("https://github.com/laurenmarietta/wingspan-with-lauren", 
+        href="https://github.com/laurenmarietta/wingspan-with-lauren"))
 ))
